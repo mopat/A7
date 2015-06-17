09696cb8092d2c0c9be2cbcfa75d7cbe25957e23
david <david.retzer00@gmail.com>
2015-06-17 15:34:35 +0200

    files created

--- frequalyzer.py
+++ frequalyzer.py
@@ -6,6 +6,67 @@ import sys
 
 from wiimote_node import *
 
+class FFTNode(Node):
+
+    nodeName = "FFT"
+
+    def __init__(self, name):
+        """self.nSamples = nSamples    # Number of Sample must be a 2^n power
+        self.aData = aData          # Amplitude data array
+        self.sRate = sRate          # Sample Rate
+        self.wFunction = wFunction  # Windowing Function
+        self.zStart = zStart        # Start of Zoom Window if Used
+        self.zStop = nSamples/2     # End of Zoom Window if Used
+        # Instantiate a plot window within an existing pyQtGraph window.
+        self.plot = win.addPlot(title="FFT")"""
+
+        terminals = {
+            'dataIn': dict(io='in'),
+            'dataOutX': dict(io='out'),
+            'dataOutY': dict(io='out'),
+        }
+
+        self.fftArray = np.array([])
+
+        #self.update(self.fftArray)
+        #self.grid_state()
+
+        #self.plot.setLabel('left', 'Amplitude', 'Volts')
+        #self.plot.setLabel('bottom', 'Frequency', 'Hz')
+
+        Node.__init__(self, name, terminals=terminals)
+
+    def process(self, **kwds):
+        # kwds will have one keyword argument per input terminal.
+
+        #size = int(self.ctrls['size'].value())
+        self.fftArray = np.append(self.fftArray, kwds['dataIn'])
+        #self.fftArray = self.fftArray[-size:]
+
+        #print (self.fftArray)
+
+
+        x = np.fft.fft(self.fftArray)
+        amplitude = np.absolute(x)
+
+        print (x)
+        print (amplitude)
+
+        return {'dataOutX': x, 'dataOutY': amplitude}
+
+
+    """def update(self, fftArray):
+        x = np.fft.fft(fftArray)
+        amplitude = np.absolute(x)
+        fScale = np.linspace(0 , 50000, self.nSamples)
+        self.plot.plot(amplitude)
+        # Calculate and set-up X axis
+        self.plot.setXRange(SampleSize/2, 0)
+
+    def grid_state(self, x = True, y = True):
+        self.plot.showGrid(x, y)"""
+
+fclib.registerNodeType(FFTNode, [('Data',)])
 
 class Analyze():
     def __init__(self):
@@ -35,7 +96,8 @@ class Analyze():
         self.createWidgets()
         self.wiimoteNode()
         self.bufferPlots()
-        self.filterNodes()
+
+        self.fftNode()
 
         self.win.show()
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
@@ -43,7 +105,7 @@ class Analyze():
 
     # create plotwidgets
     def createWidgets(self):
-        self.pw1 = pg.PlotWidget()
+        """self.pw1 = pg.PlotWidget()
         self.layout.addWidget(self.pw1, 0, 1)
         self.pw1.setYRange(0, 1024)
         self.pw1Node = self.fc.createNode('PlotWidget', pos=(150, -150))
@@ -59,7 +121,14 @@ class Analyze():
         self.layout.addWidget(self.pw3, 0, 3)
         self.pw3.setYRange(0, 1024)
         self.pw3Node = self.fc.createNode('PlotWidget', pos=(450, -150))
-        self.pw3Node.setPlot(self.pw3)
+        self.pw3Node.setPlot(self.pw3)"""
+
+        self.pw1 = pg.PlotWidget()
+        self.layout.addWidget(self.pw1, 0, 1)
+        self.pw1.setYRange(0, 1024)
+        self.pw1Node = self.fc.createNode('PlotWidget', pos=(150, -150))
+        self.pw1Node.setPlot(self.pw1)
+
 
     # create node for wiimote
     def wiimoteNode(self):
@@ -72,61 +141,30 @@ class Analyze():
     # plot the buffers for the accelerometer data
     def bufferPlots(self):
         # buffers for x, y and z-Axis
-        bufferNodeX = self.fc.createNode('Buffer', pos=(150, 0))
-        bufferNodeY = self.fc.createNode('Buffer', pos=(300, 0))
-        bufferNodeZ = self.fc.createNode('Buffer', pos=(450, 0))
+        self.bufferNodeX = self.fc.createNode('Buffer', pos=(150, 0))
+        self.bufferNodeY = self.fc.createNode('Buffer', pos=(300, 0))
+        self.bufferNodeZ = self.fc.createNode('Buffer', pos=(450, 0))
 
         # connect buffers to the plots
-        self.fc.connectTerminals(self.wiimoteNode['accelX'], bufferNodeX['dataIn'])
-        self.fc.connectTerminals(self.wiimoteNode['accelY'], bufferNodeY['dataIn'])
-        self.fc.connectTerminals(self.wiimoteNode['accelZ'], bufferNodeZ['dataIn'])
+        self.fc.connectTerminals(self.wiimoteNode['accelX'], self.bufferNodeX['dataIn'])
+        self.fc.connectTerminals(self.wiimoteNode['accelY'], self.bufferNodeY['dataIn'])
+        self.fc.connectTerminals(self.wiimoteNode['accelZ'], self.bufferNodeZ['dataIn'])
 
         # display buffer data in the plots
-        self.fc.connectTerminals(bufferNodeX['dataOut'], self.pw1Node['In'])
-        self.fc.connectTerminals(bufferNodeY['dataOut'], self.pw2Node['In'])
-        self.fc.connectTerminals(bufferNodeZ['dataOut'], self.pw3Node['In'])
+        #self.fc.connectTerminals(self.bufferNodeX['dataOut'], self.pw1Node['In'])
+        #self.fc.connectTerminals(self.bufferNodeY['dataOut'], self.pw2Node['In'])
+        #self.fc.connectTerminals(self.bufferNodeZ['dataOut'], self.pw3Node['In'])
 
+    def fftNode(self):
+        self.fftNode = self.fc.createNode('FFT', pos=(0, 0), )
+
+        self.fc.connectTerminals(self.bufferNodeX['dataOut'], self.fftNode['dataIn'])
+
+        self.fc.connectTerminals(self.fftNode['dataOutX'], self.pw1Node['In'])
+        #self.fc.connectTerminals(self.fftNode['dataOutY'], self.pw1Node['In'])
 
 if __name__ == '__main__':
     an = Analyze()
-
-class FFTNode(Node):
-
-    nodeName = "FFT"
-
-    def __init__(self,
-             win,
-             nSamples,
-             aData,
-             sRate,
-             wFunction,
-             zStart = 0):
-        self.nSamples = nSamples    # Number of Sample must be a 2^n power
-        self.aData = aData          # Amplitude data array
-        self.sRate = sRate          # Sample Rate
-        self.wFunction = wFunction  # Windowing Function
-        self.zStart = zStart        # Start of Zoom Window if Used
-        self.zStop = nSamples/2     # End of Zoom Window if Used
-        # Instantiate a plot window within an existing pyQtGraph window.
-        self.plot = win.addPlot(title="FFT")
-        self.update(aData)
-        self.grid_state()
-
-        self.plot.setLabel('left', 'Amplitude', 'Volts')
-        self.plot.setLabel('bottom', 'Frequency', 'Hz')
-
-    def update(self, aData):
-        x = np.fft.fft(aData,)
-        amplitude = np.absolute(x)
-        fScale = np.linspace(0 , 50000, self.nSamples)
-        self.plot.plot(amplitude)
-        # Calculate and set-up X axis
-        self.plot.setXRange(SampleSize/2, 0)
-
-    def grid_state(self, x = True, y = True):
-        self.plot.showGrid(x, y)
-
-fclib.registerNodeType(FFTNode, [('Data',)])
 
 
 
