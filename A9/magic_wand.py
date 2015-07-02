c1db2aca8b2d768d4203a6f312404e2d91323947
david <david.retzer00@gmail.com>
2015-07-02 15:30:54 +0200

    paintwidget changes

--- A9/magic_wand.py
+++ A9/magic_wand.py
@@ -3,6 +3,7 @@
 import time
 import sys
 import svmutil
+import random
 
 
 from wiimote_node_extended import *
@@ -11,7 +12,7 @@ from dollar import Recognizer
 from pyqtgraph.GraphicsScene import GraphicsScene
 
 circlePoints = [(269, 84), (263, 86), (257, 92), (253, 98), (249, 104), (245, 114), (243, 122), (239, 132), (237, 142), (235, 152), (235, 162), (235, 172), (235, 180), (239, 190), (245, 198), (251, 206), (259, 212), (267, 216), (275, 218), (281, 222), (287, 224), (295, 224), (301, 226), (311, 226), (319, 226), (329, 226), (339, 226), (349, 226), (352, 226), (360, 226), (362, 225), (366, 219), (367, 217), (367, 209), (367, 206), (367, 198), (367, 190), (367, 182), (367, 174), (365, 166), (363, 158), (359, 152), (355, 146), (353, 138), (349, 134), (345, 130), (341, 124), (340, 122), (338, 121), (337, 119), (336, 117), (334, 116), (332, 115), (331, 114), (327, 110), (325, 109), (323, 109), (321, 108), (320, 108), (318, 107), (316, 107), (315, 107), (314, 107), (313, 107), (312, 107), (311, 107), (310, 107), (309, 106), (308, 106), (307, 105), (306, 105), (305, 105), (304, 105), (303, 104), (302, 104), (301, 104), (300, 104), (299, 103), (298, 103), (296, 102), (295, 101), (293, 101), (292, 100), (291, 100), (290, 100), (289, 100), (288, 100), (288, 99), (287, 99), (287, 99)]
-
+squarePoints = [(193, 123), (193, 131), (193, 139), (195, 151), (197, 161), (199, 175), (201, 187), (205, 201), (207, 213), (209, 225), (213, 235), (213, 243), (215, 251), (215, 254), (217, 262), (217, 264), (217, 266), (217, 267), (218, 267), (219, 267), (221, 267), (224, 267), (227, 267), (237, 267), (247, 265), (259, 263), (273, 261), (287, 261), (303, 259), (317, 257), (331, 255), (347, 255), (361, 253), (375, 253), (385, 253), (395, 251), (403, 249), (406, 249), (408, 249), (408, 248), (409, 248), (409, 246), (409, 245), (409, 242), (409, 234), (409, 226), (409, 216), (407, 204), (407, 194), (405, 182), (403, 172), (403, 160), (401, 150), (399, 140), (399, 130), (397, 122), (397, 119), (397, 116), (396, 114), (396, 112), (396, 111), (396, 110), (396, 109), (396, 108), (396, 107), (396, 106), (396, 105), (394, 105), (392, 105), (384, 105), (376, 105), (364, 105), (350, 107), (334, 109), (318, 111), (306, 113), (294, 115), (286, 117), (278, 117), (272, 119), (269, 119), (263, 121), (260, 121), (254, 123), (251, 123), (245, 125), (243, 125), (242, 125), (241, 126), (240, 126), (238, 127), (236, 127), (232, 128), (231, 128), (231, 129), (230, 129), (228, 129), (227, 129), (226, 129), (225, 129), (224, 129), (223, 129), (222, 129), (221, 130), (221, 130)]
 
 
 class RecognizerNode(Node):
@@ -27,6 +28,7 @@ class RecognizerNode(Node):
 
         self.recognizer = Recognizer()
         self.recognizer.addTemplate('circle', circlePoints)
+        self.recognizer.addTemplate('square', squarePoints)
         self.positions = []
 
         self.last_name = None
@@ -34,15 +36,17 @@ class RecognizerNode(Node):
 
         self.win2 = pg.GraphicsView()
         #self.win2.setGeometry(300, 300, 350, 100)
+
+        self.win2.setBackgroundBrush(QtGui.QColor(255, 255, 255))
+        self.win2.setWindowTitle('Paint Widget')
+        self.win2.setGeometry(800, 600, 800, 600)
+
         self.vb = pg.ViewBox()
+
         self.win2.setCentralItem(self.vb)
 
         self.win2.show()
 
-        #self.paintWidget = QtGui.QWidget()
-        #self.paintWidget.setGeometry(800, 600, 800, 600)
-        #self.paintWidget.setWindowTitle('Paint Widget')
-        #self.paintWidget.show()
 
 
 
@@ -69,14 +73,22 @@ class RecognizerNode(Node):
         print(self.last_name)
         print(self.last_accuracy)
 
-        self.paintEvent()
+        if self.last_name == "circle":
+            self.paintCircle()
+        elif self.last_name == "square":
+            self.paintRect()
 
-    def paintEvent(self):
-        print("painting")
+    def paintCircle(self):
+        print("painting circle")
 
-        obj = Obj()
-        self.vb.addItem(obj)
+        circle = Circle()
+        self.vb.addItem(circle)
 
+    def paintRect(self):
+        print("painting rect")
+
+        rect = Rect()
+        self.vb.addItem(rect)
 
 
 fclib.registerNodeType(RecognizerNode, [('Data',)])
@@ -129,7 +141,7 @@ class PointerNode(Node):
 
 fclib.registerNodeType(PointerNode, [('Data',)])
 
-class Obj(QtGui.QGraphicsObject):
+class Circle(QtGui.QGraphicsObject):
     def __init__(self):
         QtGui.QGraphicsObject.__init__(self)
         GraphicsScene.registerObject(self)
@@ -137,8 +149,33 @@ class Obj(QtGui.QGraphicsObject):
     def paint(self, p, *args):
         #p.setPen(pg.mkPen(200,200,200))
         p.setBrush(QtGui.QColor(200, 0, 0))
+
+        p.drawEllipse(0, 0, 25, 25)
+
+    def boundingRect(self):
+        newRect = QtCore.QRectF(10, 15, 50, 40)
+
+        return newRect
+
+
+
+    def mouseClickEvent(self, ev):
+        if ev.double():
+            print("double click")
+        else:
+            print("click")
+        ev.accept()
+
+class Rect(QtGui.QGraphicsObject):
+    def __init__(self):
+        QtGui.QGraphicsObject.__init__(self)
+        GraphicsScene.registerObject(self)
+
+    def paint(self, p, *args):
+        #p.setPen(pg.mkPen(200,200,200))
+        p.setBrush(QtGui.QColor(100, 0, 0))
         #p.setBackground(QtCore.Qt.red)
-        p.drawEllipse(self.boundingRect())
+        p.drawRect(10, 15, 10, 10)
 
     def boundingRect(self):
         newRect = QtCore.QRectF(10, 15, 50, 40)
